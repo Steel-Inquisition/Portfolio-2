1b2676f8aacd484aeccb83e8edbdaf80de9946e6
Steel-Inquisition <62898596+Steel-Inquisition@users.noreply.github.com>
2021-01-11 09:58:39 -0700

    Add files via upload

--- Battleroyal/js/generateMap.js
+++ Battleroyal/js/generateMap.js
@@ -4,7 +4,10 @@ function generateMap() {
     mapName = "Island in Ocean";
     turn.scenario.push(
         { name: "Flood", description: "A wild flood comes down!", function: 'for (let i = 0; i < players.length; i++) { if (randomInt(0, 10) > 5) { let dam = 10; players[i].hp -= dam; text = `<li>The Storm deals ${dam} to ${players[i].name}</li>`; logEl.innerHTML += text; log2El.innerHTML += text;}}', last: 3 },
-        { name: "Wildlife Attack!", description: "Snarling Bears the size of monsters roam the land!", function: 'let dam = 1000; damage += dam; text = `<li>The warm sunlight gives the warrior + ${dam} damage!</li>`; logEl.innerHTML += text; log2El.innerHTML += text;', last: 3 }
+        { name: "Warm Sunlight!", description: "Warmsunlight makes you stronger!", function: 'let dam = 1000; damage += dam; text = `<li>The warm sunlight gives the warrior + ${dam} damage!</li>`; logEl.innerHTML += text; log2El.innerHTML += text;', last: 3 },
+        { name: "Prickly Jungle!", description: "The jungle is so full of stuff...", function: 'for (let i = 0; i < players.length; i++) { if (randomInt(0, 3) == 1) { let dam = randomInt(0, 10); players[i].hp -= dam; text = `<span>${players[i]} is dealt ${dam} by stumbling in the overgrown jungle! All damage equals 1.</span>`; logEl.innerHTML += text; log2El.innerHTML += text;} damage = 1; }', last: 3 },
+        { name: "Poisonious Frogs!", description: "Deadly animals that can kill you!", function: 'for (let i = 0; i < players.length; i++) { if (randomInt(1, 8) == 1) {players[i].hp = 0}', last: 3 }
+
     );
 
     // switch (randomInt(1, 3)) {
@@ -25,4 +28,5 @@ function generateMap() {
 
     return mapName;
 }
+
 
